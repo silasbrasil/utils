caa6b3af1c3954b0933860c684e1d26fc268ff2e
Silas Brasil <silasbrasil@workeduc.com>
2017-08-03 10:56:38 -0300

    Corrige nextLevelXp

--- fibonacciLevelGame.js
+++ fibonacciLevelGame.js
@@ -1,12 +1,12 @@
 /**
  * This function return the points needed
- * to got level.
- * @param {level}
- * @return {points}
+ * to got level. Usando fibonacci modified
+ * @param {number} level
+ * @return {number}
  */
-const fibonacciLevelGame = (level) => {
+const nextLevelXp = (level) => {
    return level < 1 ? 0
         : level == 1 ? 1
 		: level == 2 ? 2
-        : fibonacciM(level - 1) + fibonacciM(level - 2);
+        : nextLevelXp(level - 1) + nextLevelXp(level - 2);
 }
