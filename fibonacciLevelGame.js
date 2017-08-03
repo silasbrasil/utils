fa40a03124bc609bf640c690bd174be04ff0d22e
Silas Brasil <silasbrasil@workeduc.com>
2017-08-03 16:11:35 -0300

    Cria função getLevel

--- fibonacciLevelGame.js
+++ fibonacciLevelGame.js
@@ -1,12 +1,28 @@
 /**
- * This function return the points needed
- * to got level. Usando fibonacci modified
+ * This function return the points needed to got a specific
+ * level. Use Fibonacci Rule modified. The Level is 1
  * @param {number} level
  * @return {number}
  */
-const nextLevelXp = (level) => {
-   return level < 1 ? 0
-        : level == 1 ? 1
-		: level == 2 ? 2
-        : nextLevelXp(level - 1) + nextLevelXp(level - 2);
-}
+ const getLevelXp = (level) => {
+     return level < 2 ? 1
+ 		: level == 2 ? 2
+        : getLevelXp(level - 1) + getLevelXp(level - 2)
+ }
+
+ /**
+  * Get level of user given a XP
+  * @param {number} xp
+  * @return {number}
+  */
+ const getLevel = (xp) => {
+     let level = 1
+     let beforeXp = 0
+     while (true) {
+         let nextXp = getLevelXp(level) * 100
+         if (xp < nextXp)
+             return level
+         beforeXp = nextXp
+         level++
+     }
+ }
